913ce609127f839cc90f36f2e9b7a886145e305f
Badyl1g <wojtasgal1998@gmail.com>
2018-03-25 18:29:00 +0200

    Add tests for todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,15 +1,18 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
 const todos = [
   {
-      "text": "New todo"
+    "_id": new ObjectID(), 
+    "text": "New todo"
   },
   {
-      "text": "New todo 2"
+    "_id": new ObjectID(),
+    "text": "New todo 2"
   }
 ];
 
@@ -58,7 +61,7 @@ describe('POST /todos', () => {
         }
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(3);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(err => done(err));
@@ -74,6 +77,34 @@ describe('GET /todos', () => {
       .get('/todos')
       .expect(200)
       .expect(res => expect(res.body.todos.length).toBe(2))
+      .end(done);
+  });
+
+});
+
+describe('GET /todos/:id', () => {
+
+  it('should return todo with given id', done => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => expect(res.body.todo.text).toBe(todos[0].text))
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', done => {
+    const falseID = new ObjectID().toHexString(); // create new id that does not exist in mock data
+
+    request(app)
+      .get(`/todos/${falseID}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', done => {
+    request(app)
+      .get('/todos/123abc')
+      .expect(404)
       .end(done);
   });
 
