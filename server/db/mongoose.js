4e908a642e08bf0dd1b63dd41bbd11898961492a
Badyl1g <wojtasgal1998@gmail.com>
2018-03-26 21:09:38 +0200

    Geting tired of this

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,23 +1,24 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-let connectPath, options;
-//Check if we are on Heroku
-if (process.env.MONGODB_URI){
- connectPath = "mongodb://Wojtaszek@ds115579.mlab.com:15579/nodejs-course";
- options= {
-     auth: {
-         user: 'Wojtaszek',
-         password: 'P@ssw0rd'
-     }
- }
-} else {
- connectPath = "mongodb://localhost:27017/TodoApp";
- options = {}
-}
-mongoose.connect(connectPath, options);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
+
+// let connectPath, options;
+// //Check if we are on Heroku
+// if (process.env.MONGODB_URI){
+//  connectPath = "mongodb://Wojtaszek@ds115579.mlab.com:15579/nodejs-course";
+//  options= {
+//      auth: {
+//          user: 'Wojtaszek',
+//          password: 'P@ssw0rd'
+//      }
+//  }
+// } else {
+//  connectPath = "mongodb://localhost:27017/TodoApp";
+//  options = {}
+// }
+// mongoose.connect(connectPath, options);
 
 module.exports = { mongoose }
 
