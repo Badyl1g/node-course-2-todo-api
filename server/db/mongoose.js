ae468c193d2b2f2134752813e188e80f2649e91f
Badyl1g <wojtasgal1998@gmail.com>
2018-03-26 21:23:50 +0200

    That has to work

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,23 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
-// let connectPath, options;
-// //Check if we are on Heroku
-// if (process.env.MONGODB_URI){
-//  connectPath = "mongodb://Wojtaszek@ds115579.mlab.com:15579/nodejs-course";
-//  options= {
-//      auth: {
-//          user: 'Wojtaszek',
-//          password: 'P@ssw0rd'
-//      }
-//  }
-// } else {
-//  connectPath = "mongodb://localhost:27017/TodoApp";
-//  options = {}
-// }
-// mongoose.connect(connectPath, options);
+// mongoose.connect(process.env.PORT || 'mongodb://localhost:27017/TodoApp');
 
 const db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
