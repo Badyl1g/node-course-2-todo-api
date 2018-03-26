5ca4095552b7ec819d85e5b6d836a546da5279d9
Badyl1g <wojtasgal1998@gmail.com>
2018-03-26 21:30:26 +0200

    Everything will work

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// mongoose.connect(process.env.PORT || 'mongodb://localhost:27017/TodoApp');
-
-const db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://Wojtaszek:P@ssw0rd@ds115579.mlab.com:15579/nodejs-course'
-};
-
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose }
 
