c2b5a286136e4a5c0b2492320763be2ac1c23e06
Badyl1g <wojtasgal1998@gmail.com>
2018-03-26 20:52:09 +0200

    Changed config for heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<Wojtaszek>:<wojtas.98>@ds115579.mlab.com:15579/nodejs-course' || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+let connectPath, options;
+//Check if we are on Heroku
+if (process.env.PORT){
+ connectPath = "mongodb://<dbuser>@ds115579.mlab.com:15579/durgesh-todo";
+ options= {
+     auth: {
+         user: '<dbuser>',
+         password: '<dbpassword>'
+     }
+ }
+} else {
+ connectPath = "mongodb://localhost:27017/TodoApp";
+ options = {}
+}
+mongoose.connect(connectPath, options);
 
 module.exports = { mongoose }
+
