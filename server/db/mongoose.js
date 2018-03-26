c6f7dda962c6f96fb04414370b3a0473da170e37
Badyl1g <wojtasgal1998@gmail.com>
2018-03-26 21:16:21 +0200

    That has to work

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 // let connectPath, options;
 // //Check if we are on Heroku
@@ -19,6 +19,13 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 //  options = {}
 // }
 // mongoose.connect(connectPath, options);
+
+const db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://Wojtaszek:P@ssw0rd@ds115579.mlab.com:15579/nodejs-course'
+};
+
+mongoose.connect(db.localhost || db.mlab);
 
 module.exports = { mongoose }
 
