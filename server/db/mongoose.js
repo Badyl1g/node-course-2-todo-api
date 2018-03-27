995ae65627b8c2e9e8228a38245afc86214c254c
Badyl1g <wojtasgal1998@gmail.com>
2018-03-27 15:01:19 +0200

    Fixed database connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://Wojtaszek:P%40ssw0rd@ds115579.mlab.com:15579/nodejs-course');
 
 module.exports = { mongoose }
 
