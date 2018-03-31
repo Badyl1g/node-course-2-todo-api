2c2f96d90d3ec083dfb2edb37d1d83b472d0f2aa
Badyl1g <wojtasgal1998@gmail.com>
2018-03-31 20:13:30 +0200

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -71,7 +72,7 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.token': token,
     'tokens.access': 'auth'
   })
-  // return User.findOne({
+  // return User.findOne({    the same
   //   _id: decoded._id,
   //   tokens: {
   //     access: 'auth',
@@ -79,6 +80,21 @@ UserSchema.statics.findByToken = function(token) {
   //   },
   // })
 };
+
+UserSchema.pre('save', function(next) {
+  const user = this; // before saving change text password to hashed and salted password
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 const User = mongoose.model('User', UserSchema);
 
