efd60c25c5408fd9332bcf568ae708dbef4099e1
Badyl1g <wojtasgal1998@gmail.com>
2018-03-31 21:05:52 +0200

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,27 +4,11 @@ const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
+const { User } = require('../models/user');
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
-const todos = [
-  {
-    "_id": new ObjectID(), 
-    "text": "New todo"
-  },
-  {
-    "_id": new ObjectID(),
-    "text": "New todo 2",
-    "completed": true,
-    "completedAt": 333
-  }
-];
-
-beforeEach(done => {
-  Todo.remove({})
-    .then(() => {
-      return Todo.insertMany(todos);
-    })
-    .then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
 
@@ -190,6 +174,80 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.todo.completed).toBe(false);
         expect(res.body.todo.completedAt).toNotExist();
       })
+      .end(done);
+  });
+
+});
+
+describe('GET /users/me', () => {
+
+  it('should return user if authenticated', done => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect(res => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return a 401 if not authenticated', done => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect(res => expect(res.body).toEqual({}))
+      .end(done);
+  });
+
+});
+
+describe('POST /users', () => {
+
+  it('should create a user', done => {
+    const email = 'example@example.com';
+    const password = '123mnb!';
+
+    request(app)
+      .post('/users')
+      .send({ email, password })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers['x-auth']).toExist(); // because of the '-'
+        expect(res.body.email).toBe(email);
+      })
+      .end(err => { // using custom done() to check what has been saved to database
+        if (err) {
+          return done(err);
+        }
+        User.findOne({ email })
+          .then(user => {
+            expect(user).toExist();
+            expect(user.password).toNotBe(password);
+            done();
+          });
+      });
+  });
+
+  it('should return validation errors if request invalid', done => {
+    const invalidEmail = 'abc123';
+
+    request(app)
+      .post('/users')
+      .send({ invalidEmail })
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email in use', done => {
+    const takenEmail = users[0].email;
+    const password = 'Password123!';
+
+    request(app)
+      .post('/users')
+      .send({ takenEmail, password })
+      .expect(400)
       .end(done);
   });
 
